37816680003d97d30e17060fb3fb78e3ee051581
Hanina Syed <haninasyed@Haninas-MacBook-Pro.local>
2020-07-17 10:33:01 -0700

    mvp completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import styled from 'styled-components'
+import axios from 'axios'
+import Character from './components/Character'
+
+const Character_cards = styled.div`
+background-image: url('./images/sw-bg.jpg');
+background-size: cover;
+background-repeat: no-repeat;
+// border: 1px solid red;
+display: flex;
+flex-wrap: wrap;
+// height: 40vh;
+justify-content: space-around;
+// align-items: space-between;
+
+`
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+ 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([])
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(response => {
+      console.log(response)
+      setCharacters(response.data.results)
+    })
+    .catch(err => {
+      console.log("The data is not showing")
+    })
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <Character_cards>
+      {characters.map((character, index) => {
+        return(
+          <Character
+          key ={index}
+          name={character.name}
+          gender={character.gender}
+          hair={character.hair_color}
+          
+          />
+        )
+      }, [])}
+      </Character_cards>
+      
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const Character_card = styled.div`
+border-bottom: 3px dotted #ADB7B3;
+padding: 1%;
+width: 40%;
+// height: 10vh;
+margin: 0 auto;
+font-family: 'Red Rose';
+// color: red;
+background-color: #FFEDDF;
+
+    h1{
+        font-size: 2.8rem;
+        color: #D85A56;
+    }
+
+    h2{
+        font-size: 2.2rem;
+        color: #ADB7B3;
+    }
+
+    p{
+        font-size: 1.8rem;
+        color: #1B2230;
+    }
+`
+
+const Character = (props) => {
+    // const {characters} = props;
+    console.log(props)
+
+    return(
+        <Character_card className="character-card">
+        <h1>{props.name}</h1>
+        <h2>Gender: {props.gender}</h2>
+        <p>Hair: {props.hair}</p>
+        </Character_card>
+    )
+}
+
+export default Character
